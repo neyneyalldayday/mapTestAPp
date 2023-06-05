ee8902a7f3e6dcf1fb42b8b1dae4d095c23bc1fc
neyneyalldayday <drfrankenstein123@gmail.com>
2023-06-05 18:33:40 -0500

    spun up an app

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Map from "./components/Map";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Map />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Map.jsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import { GoogleMap, Marker, LoadScript } from "@react-google-maps/api";
+
+const containerStyle = {
+  width: "400px",
+  height: "400px",
+};
+
+export default function Map() {
+  const [latitude, setLatitude] = useState("");
+  const [longitude, setLongitude] = useState("");
+  const [markerPosition, setMarkerPosition] = useState(null);
+
+  const handleLatitudeChange = (e) => {
+    setLatitude(e.target.value);
+  };
+
+  const handleLongitudeChange = (e) => {
+    setLongitude(e.target.value);
+  };
+
+  const handleMarkerClick = () => {
+    if (latitude && longitude) {
+      const position = {
+        lat: parseFloat(latitude),
+        lng: parseFloat(longitude),
+      };
+      setMarkerPosition(position);
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        <label htmlFor="latitude">Latitude:</label>
+        <input
+          type="text"
+          id="latitude"
+          value={latitude}
+          onChange={handleLatitudeChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="longitude">Longitude:</label>
+        <input
+          type="text"
+          id="longitude"
+          value={longitude}
+          onChange={handleLongitudeChange}
+        />
+      </div>
+      <button onClick={handleMarkerClick}>Add Marker</button>
+      <LoadScript googleMapsApiKey="API_KEY">
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={markerPosition}
+          zoom={10}
+        >
+          {markerPosition && <Marker position={markerPosition} />}
+        </GoogleMap>
+      </LoadScript>
+    </div>
+  );
+}

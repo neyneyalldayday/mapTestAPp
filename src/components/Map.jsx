b786c2195a3d2e4793227de36d2593995a631451
neyneyalldayday <drfrankenstein123@gmail.com>
2023-06-06 12:35:30 -0500

    added a way to get your current location

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,4 +1,4 @@
-import  { useState } from "react";
+import  { useState, useEffect } from "react";
 import { GoogleMap, Marker, LoadScript } from "@react-google-maps/api";
 
 const containerStyle = {
@@ -11,8 +11,32 @@ function Map() {
   const [latitude, setLatitude] = useState("29.7255333");
   const [longitude, setLongitude] = useState("-98.4946");
   const [markerPosition, setMarkerPosition] = useState(null);
+  const [userLocation, setUserLocation] = useState(null);
 
+  const getUserLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          setLatitude(latitude);
+          setLongitude(longitude);
+          setUserLocation({ lat: latitude, lng: longitude });
+        },
+        (error) => {
+          console.log("Error getting user location:", error);
+        }
+      );
+    } else {
+      console.log("Geolocation is not supported by this browser.");
+    }
+  };
 
+  useEffect(() => {
+    getUserLocation();
+  }, []);
+  
+
+  
 
   const handleLatitudeChange = (e) => {
     setLatitude(e.target.value);
@@ -22,8 +46,19 @@ function Map() {
     setLongitude(e.target.value);
   };
 
+  // const handleMarkerClick = () => {
+  //   if (latitude && longitude) {
+  //     const position = {
+  //       lat: parseFloat(latitude),
+  //       lng: parseFloat(longitude),
+  //     };
+  //     setMarkerPosition(position);
+  //   }
+  // };
   const handleMarkerClick = () => {
-    if (latitude && longitude) {
+    if (userLocation) {
+      setMarkerPosition(userLocation);
+    } else if (latitude && longitude) {
       const position = {
         lat: parseFloat(latitude),
         lng: parseFloat(longitude),
@@ -54,6 +89,8 @@ function Map() {
         />
       </div>
       <button onClick={handleMarkerClick}>Add Marker</button>
+      <button onClick={getUserLocation}>Get Current Location</button>
+
       <LoadScript googleMapsApiKey='AIzaSyBo8JkbyffU2Dbv8_MDEsvcOzymPyEL0xQ'>
         <GoogleMap
           mapContainerStyle={containerStyle}
